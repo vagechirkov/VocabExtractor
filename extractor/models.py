404c02d543e68d99b59ee41d8ecec0bb843cd50b
vagechirkov <vagechirkov@gmail.com>
2022-04-30 15:25:34 +0200

    add test word model

--- extractor/models.py
+++ extractor/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    """Words and their properties"""
+    word = models.CharField(max_length=100)
+    is_known = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.word
